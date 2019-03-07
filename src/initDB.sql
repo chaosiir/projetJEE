062a6c2584282b65c0a960a37399cf302f244cbc
Nicolas Ayala <nayala@enssat.fr>
2019-03-07 11:41:22 +0100

    added script to initialize DB

---
+++ src/initDB.sql
@@ -0,0 +1,19 @@
+USE projetJEE;
+
+CREATE TABLE Students(
+	ID varchar(9) NOT NULL,
+	Gender varchar(1) NOT NULL,
+	FirstName varchar(64) NOT NULL,
+	LastName varchar(64) NOT NULL,
+	Birthdate date NOT NULL,
+	Bac varchar(16),
+	BacYear int,
+	Grade varchar(2),
+	Degree varchar(16),
+	DegreeYear int,
+	DegreeCity varchar(64),
+	RegistrationYear int,
+	EmailPro varchar(64),
+	EmailPer varchar(64),
+	PRIMARY KEY (ID)
+);
