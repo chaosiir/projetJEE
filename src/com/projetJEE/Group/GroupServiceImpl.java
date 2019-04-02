37e1ad2df665fd44c94d7ef4e49dec8ad6b8d150
Nicolas Ayala <nayala@enssat.fr>
2019-04-02 20:04:20 +0200

    javadoc for services

--- src/com/projetJEE/Group/GroupServiceImpl.java
+++ src/com/projetJEE/Group/GroupServiceImpl.java
@@ -2,7 +2,6 @@ package com.projetJEE.Group;
 
 import com.projetJEE.Student.Student;
 import com.projetJEE.Student.StudentDAOImpl;
-import com.projetJEE.Student.StudentServiceImpl;
 import com.projetJEE.User.User;
 import com.projetJEE.User.UserDAOImpl;
 
@@ -12,6 +11,10 @@ public class GroupServiceImpl implements GroupService {
 
     private static GroupServiceImpl instance;
 
+    /**
+     * Returns the GroupServiceImpl singleton
+     * @return GroupServiceImpl instance
+     */
     public static GroupServiceImpl getInstance() {
         if (instance == null) {
             synchronized (GroupServiceImpl.class) {
@@ -23,36 +26,80 @@ public class GroupServiceImpl implements GroupService {
 
     private GroupDAO groupDAO = new GroupDAOImpl(new UserDAOImpl(), new StudentDAOImpl());
 
+    /**
+     * Creates a group
+     * @param group Group to create
+     * @pre group.getID() == -1
+     * @post group.getID() != -1
+     */
     @Override
     public void newGroup(Group group) { groupDAO.create(group); }
 
+    /**
+     * Returns all groups
+     * @return A list of groups
+     */
     @Override
     public List<Group> getAllGroups() {
         return groupDAO.findAll();
     }
 
+    /**
+     * Returns groups by owner
+     * @param owner Group's owner
+     * @return A list of groups with that owner
+     */
     @Override
     public List<Group> getGroupsByOwner(User owner) { return groupDAO.findByOwner(owner); }
 
+    /**
+     * Returns a group by ID
+     * @param ID Group's ID
+     * @return The group with that ID or null if nonexistent
+     */
     @Override
     public Group getGroupByID(int ID) {
         return groupDAO.findByID(ID);
     }
 
+    /**
+     * Returns a group by name
+     * @param name Group's name
+     * @return The group with that name or null if nonexistent
+     */
     @Override
     public Group getGroupByName(String name) {
         return groupDAO.findByName(name);
     }
 
+    /**
+     * Adds a student to a group
+     * @param group Group where the student is added
+     * @param student Student to add
+     */
     @Override
     public void addStudentToGroup(Group group, Student student) { groupDAO.addStudent(group, student); }
 
+    /**
+     * Removes a student from a group
+     * @param group Group where the student is removed
+     * @param student Student to remove
+     */
     @Override
     public void removeStudentFromGroup(Group group, Student student) { groupDAO.removeStudent(group, student); }
 
+    /**
+     * Updates a group
+     * @param group Group to update
+     * Only owner, name and creation date can be updated
+     */
     @Override
     public void updateGroup(Group group) { groupDAO.update(group); }
 
+    /**
+     * Deletes a group
+     * @param group Group to delete
+     */
     @Override
     public void deleteGroup(Group group) { groupDAO.update(group); }
 }
--- src/com/projetJEE/User/UserServiceImpl.java
+++ src/com/projetJEE/User/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.projetJEE.User;
 
 import com.projetJEE.DBManager;
+import com.projetJEE.Student.StudentServiceImpl;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -12,6 +13,10 @@ public class UserServiceImpl implements UserService {
 
     private static UserServiceImpl instance;
 
+    /**
+     * Returns the UserServiceImpl singleton
+     * @return UserServiceImpl instance
+     */
     public static UserServiceImpl getInstance() {
         if (instance == null) {
             synchronized (UserServiceImpl.class) {
@@ -23,6 +28,12 @@ public class UserServiceImpl implements UserService {
 
     private UserDAO userDAO = new UserDAOImpl();
 
+    /**
+     * Creates a new user
+     * @param user User to create
+     * @pre user.getID() == -1
+     * @post user.getID() != -1
+     */
     @Override
     public void newUser(User user) {
         //hash password and answer
@@ -31,32 +42,57 @@ public class UserServiceImpl implements UserService {
         userDAO.create(user);
     }
 
+    /**
+     * Returns all users
+     * @return A list of users
+     */
     @Override
     public List<User> getAllUsers() { return userDAO.findAll(); }
 
+    /**
+     * Returns a user by ID
+     * @param ID User's ID
+     * @return The user with that ID or null if nonexistent
+     */
     @Override
     public User getUserByID(int ID) {
         return userDAO.findByID(ID);
     }
 
+    /**
+     * Returns a user by ID
+     * @param login User's login
+     * @return The user with that login or null if nonexistent
+     */
     @Override
     public User getUserByLogin(String login) {
         return userDAO.findByLogin(login);
     }
 
+    /**
+     * Updates a user
+     * @param user User to update
+     */
     @Override
     public void updateUser(User user) { userDAO.update(user); }
 
+    /**
+     * Deletes a user
+     * @param user User to delete
+     */
     @Override
     public void deleteUser(User user) { userDAO.delete(user); }
 
+    /**
+     * Hashes a string with SHA-256 in base 64
+     * @param str String to hash
+     * @return hashed string
+     */
     public String hash(String str){
-
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(str.getBytes(StandardCharsets.UTF_8));
             String hashStr = Base64.getEncoder().encodeToString(hash);
-            System.out.println("hash -> " + hashStr);
             return hashStr;
         } catch (NoSuchAlgorithmException e) { e.printStackTrace(); }
         return null;
