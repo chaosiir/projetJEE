961cb90da2bc0d8828fbaac95ec4a6a0f5da9ee0
Moncef18 <mrejebsf@enssat.fr>
2019-04-03 20:18:15 +0200

    Update fixed

---
+++ src/com/projetJEE/Servlet/AnswerServlet.java
@@ -0,0 +1,53 @@
+package com.projetJEE.Servlet;
+import com.projetJEE.User.User;
+import com.projetJEE.User.UserServiceImpl;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Answer", urlPatterns = {"/answer"})
+public class AnswerServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("post");
+
+        UserServiceImpl usimp=new UserServiceImpl();
+        User verif=usimp.getUserByLogin(request.getParameter("id"));
+
+
+        System.out.println(verif.getQuestion());
+        String x=request.getParameter("answer");
+        System.out.println(x);
+
+       if(verif.getAnswerHash().equals(usimp.hash(request.getParameter("answer")))){
+            System.out.println("verification ok");
+
+            response.sendRedirect("http://localhost:8080/projetJEE_war_exploded/update");
+
+        }
+        else {
+            System.out.println("non");
+            PrintWriter out = response.getWriter();
+            out.println("<script type=\"text/javascript\">");
+            out.println("alert('answer incorrect');");
+            out.println("</script>");
+            response.sendRedirect("http://localhost:8080/projetJEE_war_exploded/mdp");
+
+
+        }
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String pageName = "/Answer.jsp";
+        System.out.println("get");
+        RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
+        rd.forward(request, response);
+    }
+}
---
+++ web/Answer.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" >
+    <meta charset="UTF-8">
+    <title > Verification</title>
+    <link href="https://getbootstrap.com/docs/4.3/examples/sign-in/signin.css" rel="stylesheet">
+
+</head>
+<body class="text-center">
+<form class="form-signin" method="post" action="http://localhost:8080/projetJEE_war_exploded/answer" >
+    <h1 align="center">Verification</h1>
+    <%String question= (String) (request.getAttribute("question"));%>
+
+
+
+
+    <h1 align="center">Your question is : <%=question%></h1>
+
+    <label for="answer" class="sr-only">Confirm ID</label>
+    <input type="password" id="id" name="id" class="form-control" placeholder="Confirm Login" required="">
+
+    <label for="answer" class="sr-only">Answer</label>
+    <input type="password" id="answer" name="answer" class="form-control" placeholder="What is the answer to your question ?" required="">
+
+   <script>
+
+   </script>
+
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Verification</button>
+</form>
+</body>
+
+
+
+
+</html>
